d71faef797e1fd1589252496fe3be347fdd56d84
RomainDernercessian <r.dernercessian@ecoles-epsi.net>
2024-11-16 19:14:38 +0100

    Configuration pipeline CI - 7

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,25 @@
-const globals = require("globals");
-const pluginJs = require("@eslint/js");
+import globals from "globals";
+import pluginJs from "@eslint/js";
 
-/** @type {import('eslint').Linter.Config[]} */
-module.exports = [
+export default [
   {
-    ignores: ["**/*.test.js"], // Ignore les fichiers de tests
+    ignores: ["**/*.test.js"], // Ignore les fichiers de test (fichiers .test.js)
     files: ["**/*.js"], // Cible tous les fichiers JS
-    languageOptions: { sourceType: "commonjs" }, // Utilisation de CommonJS
+    languageOptions: {
+      sourceType: "commonjs", // Utilisation de CommonJS (modularité en Node.js)
+    },
   },
   {
-    languageOptions: { globals: globals.node }, // Utilise les variables globales de Node.js
+    languageOptions: {
+      globals: globals.node, // Utilisation des variables globales pour Node.js
+    },
   },
-  pluginJs.configs.recommended, // Utilise les règles recommandées d'ESLint
+  pluginJs.configs.recommended, // Utilise la configuration de règles recommandées par ESLint
   {
     rules: {
-      "no-unused-vars": "error", // Erreur pour les variables inutilisées
-      "no-undef": "error", // Erreur pour les variables non définies
-      "no-console": "error", // Erreur pour l'utilisation de console.log
+      "no-unused-vars": "error",  // Erreur si une variable est déclarée mais jamais utilisée
+      "no-undef": "error",        // Erreur si une variable est utilisée avant d'être déclarée
+      "no-console": "error",      // Erreur si console.log est trouvé dans le code
     },
   },
 ];
